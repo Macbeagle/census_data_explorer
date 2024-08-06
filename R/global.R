abcf5f9a869365d1ee9c370399cbaf4c4ee2d45d
Joshua Thomas <79579427+Macbeagle@users.noreply.github.com>
2024-08-06 12:47:41 +1000

    Update global.R

--- R/global.R
+++ R/global.R
@@ -1,5 +1,4 @@
 pkgLoad <- function( packages = "required" ) {
-  
   if( length( packages ) == 1L && packages == "required" ) {
     packages <- c( "readxl", "tidyverse", "openssl", "dplyr", "data.table",
                    "here", "httr","purrr","reshape2", "repr","plotly","scales",
@@ -7,11 +6,8 @@ pkgLoad <- function( packages = "required" ) {
                    "shinydashboard", "shinybrowser", "shinyWidgets"
     )
   }
-  
   packagecheck <- match( packages, utils::installed.packages()[,1] )
-  
   packagestoinstall <- packages[ is.na( packagecheck ) ]
-  
   if( length( packagestoinstall ) > 0L ) {
     utils::install.packages( packagestoinstall,
                              repos = "http://cran.csiro.au"
@@ -19,7 +15,6 @@ pkgLoad <- function( packages = "required" ) {
   } else {
     print( "All requested packages already installed" )
   }
-  
   for( package in packages ) {
     suppressPackageStartupMessages(
       library( package, character.only = TRUE, quietly = TRUE )
@@ -27,14 +22,11 @@ pkgLoad <- function( packages = "required" ) {
   }
 }
 pkgLoad()
-
 base_path <- here()
 dest_path <- here("data")
 extract_path <- here("data")
 output_path <- here("data")
 #load data
-
-
 # invisible(sapply(
 #   list.files('R/pubr-source', full.names = TRUE, recursive = TRUE), function(i) {
 #     source(paste0(i))
